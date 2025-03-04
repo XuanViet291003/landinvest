9179bc0c226451af15dd3e96d65864d2c86e98fd
Nguyen Xuan Duong <dd4554591@gmail.com>
2025-03-04 22:12:40 +0700

    fix hot new

--- src/components/News/categorizeNews/HotNews.jsx
+++ src/components/News/categorizeNews/HotNews.jsx
@@ -17,8 +17,14 @@ function LastNews() {
     const navigate = useNavigate();
     useEffect(() => {
         (async () => {
-            const res = await getLatestPost();
-            setPosts(res.data);
+            try {
+                const res = await getLatestPost();
+                if (typeof res.data === 'array') {
+                    setPosts(res.data);
+                }
+            } catch (err) {
+                console.log(err);
+            }
         })();
     }, []);
     const handleNavigate = (id) => {
@@ -28,32 +34,38 @@ function LastNews() {
         <div className="group-news">
             <h4 className="text-white title">Các bài viết mới nhất</h4>
             <div className="list-articles">
-                {posts.map((item, index) => (
-                    <div
-                        style={{ cursor: 'pointer' }}
-                        className="d-flex flex-column justify-content-center align-items-center "
-                        onClick={() => handleNavigate(item.PostID)}
-                    >
-                        <div key={index} className="list-articles__item">
-                            <div className="avatar">
-                                <img src={item.Images[0]?.link_image || img} alt="avatar" />
+                {posts.length > 0 ? (
+                    posts.map((item, index) => (
+                        <div
+                            style={{ cursor: 'pointer' }}
+                            className="d-flex flex-column justify-content-center align-items-center "
+                            onClick={() => handleNavigate(item.PostID)}
+                        >
+                            <div key={index} className="list-articles__item">
+                                <div className="avatar">
+                                    <img src={item.Images[0]?.link_image || img} alt="avatar" />
+                                </div>
+                                <div className="content">
+                                    <h5 className="text-white" style={{ fontSize: '16px' }}>
+                                        {item.Title}
+                                    </h5>
+                                    <p className="text-secondary" style={{ fontSize: '12px' }}>
+                                        {parse(hanleCovert(item.Content))}
+                                    </p>
+                                </div>
+                                <div className="view text-white">
+                                    <FaEye size={24} />
+                                    {item.timeView}
+                                </div>
                             </div>
-                            <div className="content">
-                                <h5 className="text-white" style={{ fontSize: '16px' }}>
-                                    {item.Title}
-                                </h5>
-                                <p className="text-secondary" style={{ fontSize: '12px' }}>
-                                    {parse(hanleCovert(item.Content))}
-                                </p>
-                            </div>
-                            <div className="view text-white">
-                                <FaEye size={24} />
-                                {item.timeView}
-                            </div>
+                            <div className="line"></div>
                         </div>
-                        <div className="line"></div>
+                    ))
+                ) : (
+                    <div className="text-white" style={{ textAlign: 'center' }}>
+                        Không có bài viết nào
                     </div>
-                ))}
+                )}
             </div>
         </div>
     );
