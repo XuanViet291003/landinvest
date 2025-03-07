31ea16548adb4dc9808c5f67c0a700136a82542f
khanhlc04 <laothucatkhanhle@gmail.com>
2025-03-07 17:53:23 +0700

    Update BoSung

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -531,24 +531,24 @@ const Map = forwardRef(
                         // dataProvinceCurrent = await getLocationInBoudingBox(vitri[0], vitri[1]);
                     }
 
-                    if (
-                        zoom >= 19 &&
-                        (!RegulationImages ||
-                            (RegulationImages[0].idProvince != dataProvinceCurrent?.provinces &&
-                                searchParams.get('type') === 'QUYHOACH_DIACHINH'))
-                    ) {
-                        // Gọi API lấy thông tin quy hoạch
-                        // const apiUrl = `https://api.quyhoach.xyz/thongtin_district/${vitri[0]}/${vitri[1]}`;
-                        // const resQuyHoach = await fetch(apiUrl);
-                        // if (!resQuyHoach.ok) throw new Error('Không thể lấy dữ liệu quy hoạch');
+                    // if (
+                    //     zoom >= 19 &&
+                    //     (!RegulationImages ||
+                    //         (RegulationImages[0].idProvince != dataProvinceCurrent?.provinces &&
+                    //             searchParams.get('type') === 'QUYHOACH_DIACHINH'))
+                    // ) {
+                    //     // Gọi API lấy thông tin quy hoạch
+                    //     const apiUrl = `https://api.quyhoach.xyz/thongtin_district/${vitri[0]}/${vitri[1]}`;
+                    //     const resQuyHoach = await fetch(apiUrl);
+                    //     if (!resQuyHoach.ok) throw new Error('Không thể lấy dữ liệu quy hoạch');
 
-                        // const dataQuyHoach = await resQuyHoach.json();
+                    //     const dataQuyHoach = await resQuyHoach.json();
 
-                        searchParams.set('type', 'QUYHOACH_DIACHINH');
-                        //searchParams.set('id', tinh.id);
-                        searchParams.set('draw', 'auto');
-                        setSearchParams(searchParams);
-                    }
+                    //     searchParams.set('type', 'QUYHOACH_DIACHINH');
+                    //     searchParams.set('id', tinh.id);
+                    //     searchParams.set('draw', 'auto');
+                    //     setSearchParams(searchParams);
+                    // }
 
                     if (zoom >= 13) {
                         debouncedHandleGetDistrict(center.lat, center.lng);
